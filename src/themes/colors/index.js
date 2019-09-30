3d636ce1d46a6081e0ee0fb95b388d48263cae17
shiva.gunasehkaran <shivagunasehkaran@rea-group.com>
2019-10-01 00:40:31 +0800

    Theme files added for color updation

---
+++ src/themes/colors/index.js
@@ -0,0 +1,12 @@
+const colors = {
+    clear: 'rgba(227,227,227,1)',
+    cardBackground: '#FFFFFF',
+    primaryText: '#000000',
+    secondaryText: '#686868',
+    divider: '#CCCCCC',
+    header: 'rgba(250,250,250,0.87)',
+    black: 'black',
+    white: 'white',
+};
+
+export default colors;
