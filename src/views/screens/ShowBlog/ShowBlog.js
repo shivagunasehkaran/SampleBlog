996ee7da077d398838fabfb5eb49bb62f081bcd3
shiva.gunasehkaran <shivagunasehkaran@rea-group.com>
2019-10-01 01:02:03 +0800

    Show blog static page completion

---
+++ src/views/screens/ShowBlog/ShowBlog.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import theme from '../../../themes';
+
+class ShowBlog extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <View style={styles.container}>
+                    <Text style={styles.title}>
+                        {('My Blog Title')}
+                    </Text>
+                    <Text style={styles.content}>
+                        {('My big blog post today is all about xyz.')}
+                    </Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        top: 100,
+        height: 300,
+        marginLeft: 10,
+        marginRight: 10,
+        borderColor: theme.colors.black,
+        borderWidth: 0.5,
+        backgroundColor: theme.colors.divider,
+    },
+    title: {
+        fontSize: 20,
+        paddingLeft: 15,
+        paddingTop: 15,
+        fontWeight: '500',
+        color: theme.colors.black,
+    },
+    content: {
+        fontSize: 15,
+        paddingLeft: 15,
+        paddingTop: 15,
+        fontWeight: '300',
+        color: theme.colors.black,
+    },
+});
+
+export default ShowBlog;
