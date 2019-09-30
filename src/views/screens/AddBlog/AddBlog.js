44860235734c46eed68eed010a5e24d6e1f5bdfc
shiva.gunasehkaran <shivagunasehkaran@rea-group.com>
2019-10-01 00:39:34 +0800

    Add blogs completion

--- src/views/screens/AddBlog/AddBlog.js
+++ src/views/screens/AddBlog/AddBlog.js
@@ -1,19 +1,91 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+import theme from '../../../themes';
 
 class AddBlog extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            title: '',
+            content: '',
+        };
     }
+
+    handleTitle = (text) => {
+        this.setState({title: text});
+    };
+
+    handleContent = (text) => {
+        this.setState({content: text});
+    };
+
+    save = (title, content) => {
+        alert('title: ' + title + ' content: ' + content);
+    };
+
+    renderBlogForm = () => {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    {('Enter Title')}
+                </Text>
+                <TextInput style={styles.input}
+                           underlineColorAndroid="transparent"
+                           placeholder="Title"
+                           placeholderTextColor="#686868"
+                           autoCapitalize="none"
+                           onChangeText={this.handleTitle} />
+                <Text style={styles.title}>
+                    {('Enter Content')}
+                </Text>
+                <TextInput style={styles.input}
+                           underlineColorAndroid="transparent"
+                           placeholder="Content"
+                           placeholderTextColor="#686868"
+                           autoCapitalize="none"
+                           onChangeText={this.handleContent} />
+
+                <TouchableOpacity
+                    style={styles.saveButton}
+                    onPress={
+                        () => this.save(this.state.title, this.state.content)
+                    }>
+                    <Text style={theme.colors.white}> {('Save')} </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    };
 
     render() {
         return (
             <View style={{flex: 1}}>
-                <Text>Add Blog</Text>
-                <Button onPress={() => this.props.navigation.navigate('Home')} title="Home" />
+                {this.renderBlogForm()}
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 100,
+    },
+    title: {
+        fontSize: 15,
+        paddingLeft: 15,
+        color: theme.colors.black,
+    },
+    input: {
+        margin: 15,
+        height: 40,
+        borderColor: theme.colors.black,
+        borderWidth: 1,
+    },
+    saveButton: {
+        backgroundColor: 'orange',
+        padding: 10,
+        margin: 15,
+        height: 40,
+    },
+});
 
 export default AddBlog;
